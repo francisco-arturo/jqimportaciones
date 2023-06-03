0c21e4f408b28b9fc2e608d0e385fa6de2f9e4c6
francisco <francisco.arturo@protonmail.com>
2023-06-03 15:25:10 -0300

    add a product detail page, displaying only 1 image

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Product
 
 def homepage(request):
     products = Product.objects.all()
     return render(request, 'homepage.html', {'products': products})
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    images = product.image.split(',')
+    return render(request, 'product.html', {'product': product, 'images': images})
