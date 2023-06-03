06fba9295054a6b780cd6914d32b6ba4e6f9dc80
francisco <francisco.arturo@protonmail.com>
2023-06-02 23:01:48 -0300

    actualizando la homepage y agregando un prod

--- core/models.py
+++ core/models.py
@@ -9,9 +9,9 @@ class Category(models.Model):
 
 class Product(models.Model):
     sku = models.IntegerField(unique=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     description = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     image = models.TextField()
 
